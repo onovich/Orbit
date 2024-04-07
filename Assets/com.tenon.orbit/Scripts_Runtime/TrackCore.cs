0fa3ad18d82438331717942e9e4d6a059f585296
onovich <hjn1110@163.com>
2024-04-07 23:27:17 +0800

    Initial commit

---
+++ Assets/com.tenon.orbit/Scripts_Runtime/TrackCore.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TenonKit.Orbit {
+
+    public class TrackCore {
+
+        TrackCoreContext ctx;
+
+        public TrackCore() {
+            ctx = new TrackCoreContext();
+        }
+    }
+
+}
